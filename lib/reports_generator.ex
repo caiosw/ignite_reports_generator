c6eaed77102b1ae631fcc2205dd48aa211f5f1b1
caiosw <caiosw@gmail.com>
2021-03-09 23:39:52 -0300

    ReportsGenerator and ReportsGenerator.Parser

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,18 +1,15 @@
 defmodule ReportsGenerator do
-  @moduledoc """
-  Documentation for `ReportsGenerator`.
-  """
+  alias ReportsGenerator.Parser
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ReportsGenerator.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
+
+  defp sum_values([id, _food_name, price], report) do
+    Map.put(report, id, report[id] + price)
+  end
+
+  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
